a18b83f470263f68756782c9c19a28328d16761a
MX6 <82836022+MX6-RBX@users.noreply.github.com>
2025-09-06 00:02:22 +0100

    Update MinersHavenOreBoostTest.lua

--- MinersHavenOreBoostTest.lua
+++ MinersHavenOreBoostTest.lua
@@ -48,6 +48,32 @@ Box.TextScaled = true
 Box.BackgroundTransparency = 1
 
 UICorner.Parent = Box
+local Suffixes = { "k", "M", "B", "T", "qd", "Qn", "sx", "Sp", "O", "N", "de", "Ud", "DD", "tdD", "qdD", "QnD", "sxD", "SpD", "OcD", "NvD", "Vgn", "UVg", "DVg", "TVg", "qtV", "QnV", "SeV", "SPG", "OVG", "NVG", "TGN", "UTG", "DTG", "tsTG", "qtTG", "QnTG", "ssTG", "SpTG", "OcTG", "NoTG", "QdDR", "uQDR", "dQDR", "tQDR", "qdQDR", "QnQDR", "sxQDR", "SpQDR", "OQDDr", "NQDDr", "qQGNT", "uQGNT", "dQGNT", "tQGNT", "qdQGNT", "QnQGNT", "sxQGNT", "SpQGNT", "OQQGNT", "NQQGNT", "SXGNTL", "USXGNTL", "DSXGNTL", "TSXGNTL", "QTSXGNTL", "QNSXGNTL", "SXSXGNTL", "SPSXGNTL", "OSXGNTL", "NVSXGNTL", "SPTGNTL", "USPTGNTL", "DSPTGNTL", "TSPTGNTL", "QTSPTGNTL", "QNSPTGNTL", "SXSPTGNTL", "SPSPTGNTL", "OSPTGNTL", "NVSPTGNTL", "OTGNTL", "UOTGNTL", "DOTGNTL", "TOTGNTL", "QTOTGNTL", "QNOTGNTL", "SXOTGNTL", "SPOTGNTL", "OTOTGNTL", "NVOTGNTL", "NONGNTL", "UNONGNTL", "DNONGNTL", "TNONGNTL", "QTNONGNTL", "QNNONGNTL", "SXNONGNTL", "SPNONGNTL", "OTNONGNTL", "NONONGNTL", "CENT", "UNCENT","inf" }  
+
+local function shorten(Input)
+	local Negative = Input < 0
+	Input = math.abs(Input)
+
+	local Paired = false
+	for i,v in pairs(Suffixes) do
+		if not (Input >= 10^(3*i)) then
+			Input = Input / 10^(3*(i-1))
+			local isComplex = (string.find(tostring(Input),".") and string.sub(tostring(Input),4,4) ~= ".")
+			Input = string.sub(tostring(Input),1,(isComplex and 4) or 3) .. (Suffixes[i-1] or "")
+			Paired = true
+			break;
+		end
+	end
+	if not Paired then
+		local Rounded = math.floor(Input)
+		Input = tostring(Rounded)
+	end
+
+	if Negative then
+		return "-"..Input
+	end
+	return Input
+end
 
 local MoneyLoopables = {
 	["Large Ore Upgarder"] ={Cap = 50e+3,Effect = nil,MinVal = nil},
@@ -125,7 +151,7 @@ local function AddTracker(ore)
 	Ui.Enabled = OreTracking
 	table.insert(OreTrackers,Ui)
 	ore.Cash.Changed:Connect(function()
-		Ui.Box.Text = ore.Cash.Value
+		Ui.Box.Text = "$"..shorten(ore.Cash.Value)
 	end)
 end
 
@@ -499,10 +525,11 @@ end
 
 if Ores then
 	Ores.ChildAdded:Connect(function(Child)
+		AddTracker(Child)
 		if OreBoost then
 			StartOreBoost(Child)
 		end
-		AddTracker(Child)
+		
 	end)
 end
 
