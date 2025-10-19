db870b5f8809ecf18a728c990aa53ecfef5f0b29
MX6 <82836022+MX6-RBX@users.noreply.github.com>
2025-10-19 20:23:45 +0100

    Delete GetAllItems.lua

--- GetAllItems.lua
+++
@@ -1,6 +0,0 @@
-local Player = game.Players.LocalPlayer
-local Popup = Player.PlayerGui.GUI.InputPrompt
-Player.Character.HumanoidRootPart.CFrame = game:GetService("Workspace").Map.DataResetModel.Model.LowerTorso.CFrame + Vector3.new(0,5,0)
-local Slot = Player.DataSlot
-wait(0.3)
-Player:Kick("Save slot "..Slot.Value.." was wiped successfully")
