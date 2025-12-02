68dcb1c572fa3a4b72915e794bf01ae871d96aff
MX6 <82836022+MX6-RBX@users.noreply.github.com>
2025-12-02 10:41:57 +0000

    Update MinersHavenAutoGift.lua

--- MinersHavenAutoGift.lua
+++ MinersHavenAutoGift.lua
@@ -1,3 +1,4 @@
+
 local Player = game.Players.LocalPlayer
 local Tycoon = Player.PlayerTycoon.Value
 local ActiveTycoon = Player.ActiveTycoon.Value
@@ -6,15 +7,12 @@ local Ores = game.Workspace.DroppedParts:FindFirstChild(Tycoon.Name)
 local GUI = Player.PlayerGui:WaitForChild("GUI")
 local AutoExchangeGift = false
 local LastGift = os.time()
-local UILib = loadstring(game:HttpGet("https://raw.githubusercontent.com/MX6-RBX/UiLib/refs/heads/main/UiLib.lua"))()
-local MainUi = UILib.new("Miners Haven Hub | MX6")
-local BoostPage = MainUi:addPage("Event","1485575235")
-local BoostSection = BoostPage:addSection("Auto Upgrade")
 local TPPos = Tycoon.Base.CFrame + Vector3.new(0,10,0)
 local Internal = Tycoon:FindFirstChild("Silicon Excavator").Model.Internal
 Internal.ProximityPrompt.RequiresLineOfSight = false
 local Furnace = Tycoon:FindFirstChild("Present Center").Model.Conv
 local ConveyorSpeed = 5
+
 
 local function Exchange()
 	local Time = os.time()-LastGift
@@ -34,16 +32,68 @@ local function Exchange()
 	keyrelease(90)
 end
 
-local AutoGift = BoostSection:addToggle("Auto Exhange Gift",false,function(Val)
-	AutoExchangeGift = Val
-	if Player.MostRecentObject.Value and Player.MostRecentObject.Value.Name == "CreatedPresent" and Val == true then
-		Exchange()
-	end
-end)
-local ConveyorSpeedSlider = BoostSection:addSlider("Conveyor Speed(Max 30)",1,5,100,function(val)
-	ConveyorSpeed = val or 5
-	AdjustSpeed.Value = ConveyorSpeed/5
-end)
+
+local Rayfield = loadstring(game:HttpGet('https://sirius.menu/rayfield'))()
+local MainUi = Rayfield:CreateWindow({
+	Name = "MX6 Miners Haven Auto Gift",
+	Icon = 0,
+	LoadingTitle = "MX6 Miners Haven Hub",
+	LoadingSubtitle = "by MX6",
+	ShowText = "Using Rayfield UI",
+	Theme = "Default", 
+
+	ToggleUIKeybind = "B",
+
+	DisableRayfieldPrompts = false,
+	DisableBuildWarnings = false, 
+
+	ConfigurationSaving = {
+		Enabled = false,
+		FolderName = nil, 
+		FileName = "MX6MHHubSettings"
+	},
+
+	Discord = {
+		Enabled = false, 
+		Invite = "discord.gg/eCQnApxxgd", 
+		RememberJoins = true
+	},
+
+	KeySystem = false, 
+	KeySettings = {
+		Title = "This Is Just A Test",
+		Subtitle = "Key System",
+		Note = "Key Is FreeKeys",
+		FileName = "MX6MinersHavenHubKeys", 
+		SaveKey = true, 
+		GrabKeyFromSite = false, 
+		Key = {"FreeKeys"}
+	}
+})
+
+local AutoPage = MainUi:CreateTab("Gift", 130772689610761) 
+local AutoRebirthSection = AutoPage:CreateSection("Auto Gift")
+local AutoRebithToggle = AutoPage:CreateToggle({
+	Name = "Auto farm gifts",
+	CurrentValue = false,
+	Flag = "",
+	Callback = function(Value)
+		AutoExchangeGift = Value
+	end,
+})
+local ConveyorSpeedSlider = AutoPage:CreateSlider({
+	Name = "Conveyor speed",
+	Range = {0, 100},
+	Increment = 1,
+	Suffix = "Conveyor Speed",
+	CurrentValue = 5,
+	Flag = "ConveyorSpeed", -- A flag is the identifier for the configuration file, make sure every element has a different flag if you're using configuration saving to ensure no overlaps
+	Callback = function(Value)
+		ConveyorSpeed = Value or 5
+		AdjustSpeed.Value = ConveyorSpeed/5
+	end,
+})
+
 game.Workspace.ChildAdded:Connect(function(Child)
 	if Child.Name == "CreatedPresent" and (Child.Position-Furnace.Position).Magnitude <= 20 then
 		if not AutoExchangeGift then return end
