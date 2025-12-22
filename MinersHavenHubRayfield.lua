5d929652279ccca311eb45cfdeaf0d93aa879ca3
MX6 <82836022+MX6-RBX@users.noreply.github.com>
2025-12-22 16:33:13 +0000

    Update MinersHavenHubRayfield.lua

--- MinersHavenHubRayfield.lua
+++ MinersHavenHubRayfield.lua
@@ -2,7 +2,6 @@ local Chat = game:GetService("TextChatService")
 local channel = Chat:WaitForChild('TextChannels').RBXGeneral
 local Player = game.Players.LocalPlayer
 local Rayfield = loadstring(game:HttpGet('https://sirius.menu/rayfield'))()
-local webhookurl = "https://discord.com/api/webhooks/1449038179901374566/aGADbecg6S-xLi0G5jzZrJPge-WIGv_CYoOte_uFZ3eUQJ9ELhnNmodCunAw5VMu-giz"
 local MainUi = Rayfield:CreateWindow({
 	Name = "MX6 Miners Haven Hub",
 	Icon = 0,
@@ -207,96 +206,6 @@ local function shorten(Input)
 	end
 	return Input
 end
-local Sf = ""
-if Player:FindFirstChild("SecondSacrifice") then 
-	Sf = "S+"
-elseif Player:FindFirstChild("Sacrificed") then
-	Sf = "s-"
-else
-	Sf = ""
-end
-local PlayerLifeData = Sf..tostring(Player.Rebirths.Value+1)
-local SlotInfo = Player.DataSlot.Value
-local RP = Player.Points.Value
-local Shards = Player.Shards.Value
-local Uc = Player.Crystals.Value
-local embedData = {
-	["title"] = "Script execution",
-	["description"] = Player.DisplayName .. " (@" .. Player.Name .. ") executed the Miners Haven Hub Script",
-	["color"] = 65280,
-	["fields"] = {
-		{
-			["name"] = "Profile",
-			["value"] = '[View Profile](https://www.roblox.com/users/'..Player.UserId..'/profile)',
-			["inline"] = true
-		},
-		{
-			["name"] = "Player Life",
-			["value"] =PlayerLifeData, -- UTC timestamp for when the game ended
-			["inline"] = true
-		},
-		{
-			["name"] = "RP",
-			["value"] = shorten(RP), -- UTC timestamp for when the game ended
-			["inline"] = true
-		},
-		{
-			["name"] = "Crystals",
-			["value"] = shorten(Uc), -- UTC timestamp for when the game ended
-			["inline"] = true
-		},
-		{
-			["name"] = "Shards",
-			["value"] = shorten(Shards), -- UTC timestamp for when the game ended
-			["inline"] = true
-		},
-		{
-			["name"] = "Slot",
-			["value"] = "Slot "..SlotInfo, -- UTC timestamp for when the game ended
-			["inline"] = true
-		},
-		{
-			["name"] = "Server Time",
-			["value"] = os.date("!%x %X"), -- UTC timestamp for when the game ended
-			["inline"] = true
-		},
-	},
-	["footer"] = {
-		["text"] = "MX6 Script logger"
-	}
-}
-function SendMessageEMBED(url, embed)
-	local http = game:GetService("HttpService")
-	local headers = {
-		["Content-Type"] = "application/json"
-	}
-	local data = {
-		["embeds"] = {
-			{
-				["title"] = embed.title,
-				["description"] = embed.description,
-				["color"] = embed.color,
-				["fields"] = embed.fields,
-				["footer"] = {
-					["text"] = embed.footer.text
-				}
-			}
-		}
-	}
-	local body = http:JSONEncode(data)
-	local response = request({
-		Url = webhookurl,
-		Method = "POST",
-		Headers = headers,
-		Body = body
-	})
-	print("Thanks for using MX6 Miners Haven Hub")
-end
-
-SendMessageEMBED(webhookurl, embedData)
-
-
-
 
 local MoneyLoopables = {
 	["Large Ore Upgarder"] ={Cap = 50e+3,Effect = nil,MinVal = nil},
