fa272fb1e5b0219140037dfbf0de73309612e043
MX6 <82836022+MX6-RBX@users.noreply.github.com>
2026-04-04 14:16:20 +0000

    Update MinersHavenHubRayfield.lua

--- MinersHavenHubRayfield.lua
+++ MinersHavenHubRayfield.lua
@@ -588,7 +588,7 @@ local function CollectEggs()
 			local prompt = Egg:FindFirstChildWhichIsA("ProximityPrompt", true)
 			if prompt then
 				Player.Character.HumanoidRootPart.CFrame = v.CFrame
-				wait(0.2)
+				wait(0.1)
 				fireproximityprompt(prompt)
 				task.wait(0.3) 
 			end
